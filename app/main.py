afb543c6cc6a2389d5861d13c571aa0b120000d7
wotjr1649 <wotjr980321@naver.com>
2024-05-01 06:32:05 +0900

    소셜 로그인 및 db 연동 구현 완료 비동기 처리 완료

--- app/main.py
+++ app/main.py
@@ -5,20 +5,22 @@ from routes.login import login_routers
 # FastAPI
 app = FastAPI()
 
-
+# CORS
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 
 # 라우터 등록
 app.include_router(login_routers, prefix="/login")
 
 
 if __name__ == "__main__":
-    import uvicorn
+    print("hello let's")
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # import uvicorn
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
