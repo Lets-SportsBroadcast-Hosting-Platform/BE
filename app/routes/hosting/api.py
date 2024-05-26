fe836748f6a375a8d5f5130e60718db4989fdf52
semin-pk <125566649+semin-pk@users.noreply.github.com>
2024-05-26 16:28:56 +0900

    feat: update sso

--- app/routes/hosting/api.py
+++ app/routes/hosting/api.py
@@ -1,22 +1,34 @@
 from database import get_db
-from fastapi import Depends, HTTPException
-from models.hosting_table import HostingData
-from routes.hosting.api_helper import insert_hosting_table, read_hosting_table, read_hosting_tables
+import io
+from fastapi import Depends, HTTPException, File, UploadFile
+from typing import List
+from .models.hosting_models import HostinginsertModel
+from routes.hosting.api_helper import (
+    insert_hosting_table, 
+    read_hosting_table, 
+    read_hosting_tables, 
+    update_hosting_table, 
+    delete_hosting_table,
+    update_storeimage,
+    make_hosting_data)
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
 # 클라이언트에서 호스팅 정보를 받아 db에 등록하는 api 함수
-async def make_hosting(hostingdata: HostingData, db: AsyncSession = Depends(get_db)):
-    print(hostingdata.game_start_date)
-    if await insert_hosting_table(hostingdata.to_HostingModel(), db):
+async def make_hosting(hostinginsertmodel: HostinginsertModel,  photos: List[UploadFile] = File(...), db: AsyncSession = Depends(get_db)):
+    hosting_data = make_hosting_data(hostinginsertmodel)
+    print(hosting_data)
+    update_storeimage(hosting_data.business_no,len(photos), hosting_data.screen_size)
+
+    #print(hostingdata.game_start_date)
+    if await insert_hosting_table(hostinginsertmodel, db):
         return "호스팅 되었습니다."
     else:
         raise HTTPException(status_code=200, detail=400)
 
 
 # 클라이언트에서 호스팅 id를 받아 응답하는 함수
-async def read_hostings(hosting_name: int, db: AsyncSession = Depends(get_db)):
-    result = await read_hosting_tables(hosting_name, db)
+async def read_hostings(business_no: int, db: AsyncSession = Depends(get_db)):
+    result = await read_hosting_tables(business_no, db)
     if result:
         return result
     else:
@@ -28,3 +40,19 @@ async def read_hosting(hosting_id: int, db: AsyncSession = Depends(get_db)):
         return result
     else:
         raise HTTPException(status_code=200, detail=400)
+    
+async def update_hosting(hosting_id: int, db: AsyncSession = Depends(get_db)):
+    result = await update_hosting_table(hosting_id, db)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=200, detail=400)
+    
+async def delete_hosting(hosting_id: int, db: AsyncSession = Depends(get_db)):
+    result = await delete_hosting_table(hosting_id, db)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=200, detail=400)
+    
+
