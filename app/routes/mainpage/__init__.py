32466ab7386a8aceeee3a0cee4b9f49df0430446
semin-pk <tpals11149@gmail.com>
2024-05-29 22:07:01 +0900

    change mainpage api

--- app/routes/mainpage/__init__.py
+++ app/routes/mainpage/__init__.py
@@ -3,4 +3,4 @@ from routes.mainpage.api import read_hostings
 
 
 #호스팅 읽기 - 사업자번호로 비교 호스팅 리스트(Read)
-mainpage_hosting_read_route = APIRoute(path="/hostings", endpoint=read_hostings, methods=["GET"])
+mainpage_hosting_read_route = APIRoute(path="/party", endpoint=read_hostings, methods=["GET"])
